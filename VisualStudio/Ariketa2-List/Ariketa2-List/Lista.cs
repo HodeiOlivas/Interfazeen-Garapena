6ba49b708f0806d4393af4124b875f37ac42a3a2
Hodei Olivas <olivas.hodei@uni.eus>
2022-10-03 12:29:47 +0200

    .

--- VisualStudio/Ariketa2-List/Ariketa2-List/Lista.cs
+++ VisualStudio/Ariketa2-List/Ariketa2-List/Lista.cs
@@ -25,7 +25,7 @@ namespace Ariketa2_List
             public static float eragiketa(List<Lista> zenbakiak)
             {
                 float emaitza = 0;
-            emaitza = (zenbakiak[0].zenb + (2 * zenbakiak[1].zenb) + (3 * zenbakiak[2].zenb) + (4 * zenbakiak[3].zenb)) / 4;
+            emaitza = (zenbakiak[0].zenb + (2 * zenbakiak[1].zenb2) + (3 * zenbakiak[2].zenb3) + (4 * zenbakiak[3].zenb4)) / 4;
             return emaitza;
             }
         }
