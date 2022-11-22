7e864780ab43fb287721e2c66ad04c82c3027a57
Hodei Olivas <olivas.hodei@uni.eus>
2022-11-22 10:20:08 +0100

    Web Application bukatuta

---
+++ VisualStudio/WebApplication1/WebApplication1/Controllers/KaixoMunduaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace WebApplication1.Controllers
+{
+    public class KaixoMunduaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult OngiEtorri(string izena = "Hodei", int zenbat = 1) {
+
+            ViewBag.Mezua = "Hello " + izena;
+            ViewBag.Zenbat = zenbat;
+            return View();
+
+         //   return HttpUtility.HtmlEncode("Kaixo " + izena + ", zenbat da: " + zenbat);
+        
+        
+        }
+    }
+}
